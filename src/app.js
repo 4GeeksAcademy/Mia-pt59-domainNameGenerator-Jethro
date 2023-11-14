5e099d3a2c38d451b7560bdf42eb6ab97f8993f5
TheJeth <145587508+TheJeth@users.noreply.github.com>
2023-11-14 00:00:47 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // document.getElementById("domain").innerHTML = finalDomain;
+
+  let companyName = ["Jeth", "Company", "glory", "whitehouse"];
+  let domainName = ["gmail", "yahoo", "hotmail", "tesla", "bmw", "nasa", "gov"];
+
+  let finalCompanyName = Math.floor(Math.random() * companyName.length);
+  let finalDomainName = Math.floor(Math.random() * domainName.length);
+
+  let finalDomain = [];
+
+  for (let i = 0; i < companyName.length; i++) {
+    for (let j = 0; j < domainName.length; j++) {
+      finalDomain.push(companyName[i] + "@" + domainName[j] + ".com");
+
+      console.log(finalDomain);
+    }
+  }
+  let element = document.querySelector("#domain");
+  for (let i = 0; i < finalDomain.length; i++) {
+    element.innerHTML += "<li>" + finalDomain[i] + "</li>";
+  }
 };
